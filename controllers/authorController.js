f471ac89733dd995fa51ae8c870d18cdf9d85bd9
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-02-12 19:58:43 +0300

    Added author's and genre's creation forms

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,6 +2,9 @@ var Author = require("../models/author");
 var Book = require("../models/book");
 var async = require("async");
 
+const {body, validationResult} = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
+
 exports.author_list = function(req, res, next) {
 
   Author.find()
@@ -41,12 +44,47 @@ exports.author_detail = function(req, res, next) {
 };
 
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render("author_form", {title: "Create Author"});
 };
 
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+    
+    body("first_name").isLength({min: 1}).trim().withMessage("First name must be more then nothing.")
+        .isAlphanumeric().withMessage("First name has non-alphanumeric characters."),
+
+    body("family_name").isLength({min: 1}).trim().withMessage("Family name must be more then nothing.")
+        .isAlphanumeric().withMessage("Family name has non-alphanumeric characters."),
+
+    body("date_of_birth", "Invalid date of birth").optional({checkFalsy: true}).isISO8601(),
+    body("date_of_death", "Invalid date of death").optional({checkFalsy: true}).isISO8601(),
+
+    sanitizeBody("first_name").escape(),
+    sanitizeBody("family_name").escape(),
+    sanitizeBody("date_of_birth").toDate(),
+    sanitizeBody("date_of_death").toDate(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            res.render("author_form", {title: "Create Author", author: req.body, errors: errors.array()});
+            return;
+        } else {
+            var author = new Author({
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death
+            });
+
+            author.save((err) => {
+                if (err) { return next(err); } 
+                res.redirect(author.url);
+            });
+        }
+    }
+
+];
 
 exports.author_delete_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Author delete GET');
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 var Genre = require('../models/genre');
 var Book = require('../models/book');
 var async = require('async');
+const validator = require('express-validator'); 
 
 exports.genre_list = function(req, res, next) {
 
@@ -40,15 +41,42 @@ exports.genre_detail = function(req, res, next) {
 
 };
 
-// Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', {title: "Create Genre"});
 };
 
-// Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+    
+    validator.body("name", "Genre name is required").isLength({min: 1}).trim(),
+    validator.sanitizeBody("name").escape(),
+
+    (req, res, next) => {
+        const errors = validator.validationResult(req);
+
+        var genre = new Genre({
+            name: req.body.name
+        });
+
+        if (!errors.isEmpty()) {
+            res.render("genre_form", {title: "Create Genre", genre: genre, errors: errors.array()})
+            return;
+        } else {
+
+            Genre.findOne({'name': req.body.name})
+                .exec((err, found_genre) => {
+                    if (err) { return next(err); }
+                    if (found_genre) { res.redirect(found_genre.url); }
+
+                    genre.save((err) => {
+                        if (err) { return next(err); }
+                        else res.redirect(genre.url);
+                    });
+                });
+
+        }
+    }
+
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
